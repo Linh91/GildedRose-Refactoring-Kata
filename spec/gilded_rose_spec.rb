0343359bcf7030cadb036068edc3a95a2370e9bd
Ngoc L Ha <sammy_nlh@hotmail.com>
2017-07-10 14:51:16 +0100

    Item test - sell by date has passed

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -19,6 +19,20 @@ describe GildedRose do
       GildedRose.new(items).update_quality()
       expect(items[0].quality).to eq 1
     end
+
+    it "when item sell_in date has passed, quality reduces twice as fast" do
+      items = [Item.new("foo", 0, 10)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to eq 8
+      expect(items[0].sell_in).to eq -1
+    end
+
+    it "when item sell_in date has passed, quality reduces twice as fast" do
+      items = [Item.new("foo", 2, 0)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to eq 0
+      expect(items[0].sell_in).to eq 1
+    end
   end
 
   describe "Aged Brie" do
@@ -146,5 +160,6 @@ describe GildedRose do
       expect(items[0].sell_in).to eq 21
     end
   end
+
 
 end
