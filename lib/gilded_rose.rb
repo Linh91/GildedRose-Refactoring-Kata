bb0cc2a59812403e7b2ee0ca2f4f8ad8fde94e3a
Ngoc L Ha <sammy_nlh@hotmail.com>
2017-07-15 16:07:17 +0100

    Replaced if statement with hash input

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -20,22 +20,6 @@ class GildedRose
       updated_product = (PRODUCTS[item.name] || Normal).new(item.name, item.quality, item.sell_in)
       updated_product.item_update
       return updated_product
-    #   if item.name == "Aged Brie"
-    #     @items[0] = AgedBrie.new(@items[0].name, @items[0].quality, @items[0].sell_in)
-    #     @items[0].item_update
-    #   elsif item.name == "Sulfuras, Hand of Ragnaros"
-    #     @items[0] = Sulfuras.new(@items[0].name, @items[0].quality, @items[0].sell_in)
-    #     @items[0].item_update
-    #   elsif item.name == "Backstage passes to a TAFKAL80ETC concert"
-    #     @items[0] = BackstagePass.new(@items[0].name, @items[0].quality, @items[0].sell_in)
-    #     @items[0].item_update
-    #   elsif item.name == "Conjured"
-    #     @items[0] = Conjured.new(@items[0].name, @items[0].quality, @items[0].sell_in)
-    #     @items[0].item_update
-    #   else
-    #     @items[0] = Normal.new(@items[0].name, @items[0].quality, @items[0].sell_in)
-    #     @items[0].item_update
-    #   end
     end
   end
 end
