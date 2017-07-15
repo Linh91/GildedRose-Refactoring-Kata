625462418a4df986281e75d4d94fdd89a3ada211
Ngoc L Ha <sammy_nlh@hotmail.com>
2017-07-15 18:55:02 +0100

    Refactored Sulfuras class - tested with pry

--- lib/sulfuras.rb
+++ lib/sulfuras.rb
@@ -1,10 +1,4 @@
 class Sulfuras
-  attr_reader :quality, :sell_in, :name
-
-  def initialize(name, quality, sell_in)
-    @name, @quality, @sell_in = name, quality, sell_in
-  end
-
-  def item_update
+  def item_update(item)
   end
 end
